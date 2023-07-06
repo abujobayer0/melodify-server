ab1cb44b11f9534d0366ba083151465c11df1d40
zubayermunna <muna700064@gmail.com>
2023-07-06 23:56:19 +0600

    added

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const express = require("express");
 require("dotenv").config();
 const cors = require("cors");
 const stripe = require("stripe")(process.env.PAYMENT_SECRET);
-
+const satelize = require('satelize');
 const app = express();
-const PORT = process.env.PORT || 7000;
+const PORT = process.env.PORT || 8000;
 
 // Middleware
 app.use(cors());
@@ -41,6 +41,21 @@ async function run() {
       const user = req.body;
       const result = await usersCollection.insertOne(user);
       res.send(result);
+    });
+
+    app.get('/geolocation', (req, res) => {
+      const clientIP = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+      console.log(clientIP);
+    
+      satelize.satelize({ ip:clientIP }, (err, geoData) => {
+        if (err) {
+          console.error('Error retrieving geolocation:', err);
+          res.status(500).json({ error: 'Failed to retrieve geolocation' });
+        } else {
+          console.log(geoData);
+          res.send(geoData);
+        }
+      });
     });
 
     app.get("/users", async (req, res) => {
